27aec60d1fc00c99b74941040e6f4b9df7827b18
harysz <arnodlas.valasinas@gmail.com>
2019-07-16 10:04:22 +0300

    last commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,10 +5,11 @@ const Footer=(props)=>{
         <div className='footer'>
             <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using </p>
             <div className='wrapper'>
+                <ul className='pages'>
                     {props.pages.map((item,i)=>{
-                    return(<div className='item' key={i}>{item}</div>)
+                    return(<li className='item' key={i}>{item}</li>)
                     })}
-                
+                    </ul>
                 <div className='logo'>
                 <h2 className='title'>@ </h2>
                 {props.title.map((item,i)=>{return <h2 key={i}className='title'> {item}</h2>})}
